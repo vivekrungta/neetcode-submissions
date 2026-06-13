a53763e0c65cb3c5e887b2aaad54003d76d29d55
Vivek <vivekrungta01@gmail.com>
2026-06-13 23:09:12 +0530

    Add: count-squares - submission-0

---
+++ Data Structures & Algorithms/count-squares/submission-0.java
@@ -0,0 +1,25 @@
+class CountSquares {
+    Map<String,Integer> countMap;
+    Set<int[]> points;
+
+    public CountSquares() {
+        countMap = new HashMap<>();
+        points= new HashSet<>();
+    }
+
+
+    public void add(int[] point) {
+        points.add(point);
+        String key = point[0]+"#"+point[1];
+        countMap.put(key,countMap.getOrDefault(key,0)+1);
+    }
+    
+    public int count(int[] point) {
+        int sum=0;
+        for(int kPoint[]:points){
+            if(kPoint[0]==point[0] || kPoint[1]==point[1] || Math.abs(kPoint[0]-point[0])!=Math.abs(kPoint[1]-point[1])) continue;
+            sum+=countMap.getOrDefault(point[0]+"#"+kPoint[1],0)*countMap.getOrDefault(kPoint[0]+"#"+point[1],0);
+        }
+        return sum;
+    }
+}
