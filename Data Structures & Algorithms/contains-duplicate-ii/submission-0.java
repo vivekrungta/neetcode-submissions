93eea3d47ff990fea6bc450cf17bcbe00e56f322
Vivek <vivekrungta01@gmail.com>
2026-06-14 17:02:21 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> st = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(st.contains(nums[i])) return true;
+            st.add(nums[i]);
+            if(st.size()>k){
+                st.remove(nums[i-k]);
+            }
+        }
+        return false;
+    }
+}
