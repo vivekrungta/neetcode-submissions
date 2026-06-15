2dc49ce435e737428100b53d68c40e5f62cc8bde
Vivek <vivekrungta01@gmail.com>
2026-06-15 19:59:30 +0530

    Add: minimum-height-trees - submission-3

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-3.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> g[] = new List[n];
+        if(n==1) {
+            return Arrays.asList(0);
+        }
+        for(int i=0;i<n;i++){
+            g[i]=new ArrayList<>();
+        }
+        for(int[] edge:edges){
+            g[edge[0]].add(edge[1]);
+            g[edge[1]].add(edge[0]);
+        }
+        List<Integer> leaves = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(g[i].size()==1){
+                leaves.add(i);
+            }
+        }
+        while(n>2){
+            n=n-leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for(Integer leave:leaves) {
+                int d = g[leave].get(0);
+                g[d].remove(leave);
+                    if(g[d].size()==1){
+                        newLeaves.add(d);
+                    }
+            }
+            leaves=newLeaves;
+        }
+        return leaves;
+    }
+}
